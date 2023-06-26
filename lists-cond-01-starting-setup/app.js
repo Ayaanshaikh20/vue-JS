790976566e5c632e8a526235a0bdd3f5768c014f
Ayaanshaikh20 <email@youremail.com>
2023-06-26 19:10:26 +0530

    end of day 26 june

--- lists-cond-01-starting-setup/app.js
+++ lists-cond-01-starting-setup/app.js
@@ -1,7 +1,21 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return { 
+      goals: [],
+      enteredgoalname: '',
+      person: {name: "ayaan", age: 22} 
+    };
   },
+  methods: {
+    addGoals() {
+      console.log('Ayaan')
+      this.goals.push(this.enteredgoalname);
+      this.enteredgoalname = '';
+    },
+    removeGoals(indx) {
+      this.goals.splice(indx, 1)
+    }
+  }
 });
 
 app.mount('#user-goals');
