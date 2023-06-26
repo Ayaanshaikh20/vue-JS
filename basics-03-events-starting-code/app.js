53cd1da9705eb090905487d46a57945fc76da491
Ayaanshaikh20 <email@youremail.com>
2023-06-26 12:03:13 +0530

    setname

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,8 +2,20 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: ''
     };
   },
+  methods: {
+    setName(event) {
+      this.name = event.target.value
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    minus(num) {
+      this.counter = this.counter - num;
+    }
+  }
 });
 
 app.mount('#events');
