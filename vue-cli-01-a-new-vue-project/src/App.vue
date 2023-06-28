338f9cce8e9a84cc3fae7153d01ab0f720a527a7
Ayaanshaikh20 <email@youremail.com>
2023-06-28 19:02:09 +0530

    vue commit first day

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <section>
+        <h2>My Friends</h2>
+        <ul>
+            <li></li>
+        </ul>
+    </section>
+    
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            friends: [
+                {
+                    id: 'Ayaan',
+                    name: 'Ayaan Shaikh',
+                    phone: '9594548987',
+                    email: 'ayaanshaikh220301@gmail.com'
+                },
+                {
+                    id: 'Himanshu',
+                    name: 'Himanshu Khond',
+                    phone: '9535454334',
+                    email: 'himanshu.k@gmail.com'
+                }
+            ]
+        }
+    }
+}
+</script>
